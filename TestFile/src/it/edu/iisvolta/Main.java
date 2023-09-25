e041b5ad5940c1cffff9fe42f5cb422d07eb2a51
Colluti <Colluti@TDP10>
2023-09-25 20:52:49 +0200

    versione con più dati per ogni riga

--- TestFile/src/it/edu/iisvolta/Main.java
+++ TestFile/src/it/edu/iisvolta/Main.java
@@ -13,11 +13,11 @@ public class Main {
 		
 		//*****  SCRITTURA SU UN FILE *****
 		try {
-			FileWriter fw=new FileWriter("prodotti.txt");	//crea un file di nome prodotti.txt
-			fw.write("Pane\n");			//scrive una riga nel file
-			fw.write("Pasta\n");
-			fw.write("Biscotti\n");
-			fw.write("Salame\n");
+			FileWriter fw=new FileWriter("prodotti.csv");	//crea un file di nome prodotti.txt
+			fw.write("Pane;3.50;20\n");			//scrive una riga nel file
+			fw.write("Pasta;1.60;45\n");
+			fw.write("Biscotti;4.70;80\n");
+			fw.write("Salame;35;37\n");
 			fw.close();					//chiude il file	
 			System.out.println("File correttamente creato");
 		} catch (IOException e) {
@@ -26,13 +26,19 @@ public class Main {
 		}
 		
 		//*****  LETTURA DA UN FILE *****
-		File f=new File("prodotti.txt");
+		File f=new File("prodotti.csv");
+		String[] dati;
 		try {
 			Scanner sc=new Scanner(f);		//questo scanner non legge dalla tastiera, bensì dal file f
 			String riga;
 			while (sc.hasNextLine()) { //ripeti finché ci sono righe da leggere
 				riga=sc.nextLine();		//leggi una riga dal file
-				System.out.println("Trovato prodotto: "+riga);
+				dati=riga.split(";");	//divide la stringa in più pezzi usando il carattere ";" come separatore
+				System.out.println("Trovato prodotto:\n");
+				System.out.println("  Nome: "+dati[0]);
+				System.out.println("  Prezzo: "+dati[1]);
+				System.out.println("  Quantità: "+dati[2]);
+				System.out.println("\n");
 			}
 			sc.close();  //chiudi il file
 			System.out.println("\n\nLettura terminata.");
